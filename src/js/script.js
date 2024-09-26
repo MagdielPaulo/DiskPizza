dcc9a2b328f22941eda83bd1ec29fc35a8543d45
MagdielPaulo <magdielpaulopy@gmail.com>
2024-09-26 17:17:51 -0300

    problemresponsiv

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,7 @@
-// const menu = document.querySelector('.menu');
-// const NavMenu = document.querySelector('.nav-menu');
+const mobileBtn = document.getElementById('mobile-btn');
+const navMenu = document.getElementById('id_list');
 
-// menu.addEventListener('click', function() {
-//     menu.classList.toggle('ativo');
-//     NavMenu.classList.toggle('ativo');
-// });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Selecionando os elementos .menu e .nav-menu
-//     const menuIcon = document.querySelector('.menu');
-//     const navMenu = document.querySelector('.nav-menu');
-
-//     // Verifique se os elementos estão sendo encontrados
-//     console.log(menuIcon); // Certifique-se de que o elemento .menu seja encontrado
-//     console.log(navMenu);  // Certifique-se de que o elemento .nav-menu seja encontrado
-
-//     // Se ambos forem encontrados, adicione o evento de clique
-//     if (menuIcon && navMenu) {
-//         menuIcon.addEventListener('click', function() {
-//             console.log('Menu clicado');
-//             menuIcon.classList.toggle('ativo');
-//             navMenu.classList.toggle('ativo');
-//         });
-//     } else {
-//         console.error('Elemento menuIcon ou navMenu não encontrado no DOM.');
-//     }
-// });
-
-const menu = document.querySelector('.menu');
-const navMenu = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    navMenu.classList.toggle('ativo');
+mobileBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('ativo'); /* Ativa a classe 'ativo' no menu */
+  mobileBtn.classList.toggle('ativo'); /* Alterna o estilo do botão hambúrguer */
 });
